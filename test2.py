05f82c04d0a6748aeae8e8fa1ab6b58baea31c21
shinduegki <97738423+shinduegki@users.noreply.github.com>
2022-01-22 22:40:13 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,11 +2,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import f1_score,accuracy_score,recall_score,precision_score
-
-
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
+from pytictoc import TicToc
 
+t = TicToc()
+
+t.tic() 
 x_train,x_test,y_train,y_test = train_test_split(X,Y,random_state=4) #★★★★
 
 #forest_reg = DecisionTreeClassifier(random_state=42)
@@ -18,7 +20,7 @@ forest_reg.fit(x_train, y_train)
 
 print(forest_reg.score(x_train, y_train)) 
 print(forest_reg.score(x_test, y_test)) 
-
+t.toc() 
 
 #y_pred = forest_reg.predict(x_test) #★★★★
 #accuracy = confusion_matrix(y_test,y_pred) #★★★★
